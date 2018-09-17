789e18bb2fd2943ca79883613870299a304184e4
SeniorZhai <developer.zhaitao@gmail.com>
2018-09-17 15:07:19 +0800

    base coroutine

--- app/src/main/java/com/coroutine/example/MainActivity.kt
+++ app/src/main/java/com/coroutine/example/MainActivity.kt
@@ -2,11 +2,47 @@ package com.coroutine.example
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.base
+import kotlinx.android.synthetic.main.activity_main.tv
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
+  var index = 0
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    base.setOnClickListener {
+      base()
+    }
+  }
+
+  private fun base() {
+    runBlocking {
+      val string = StringBuffer()
+      val a = async {
+        log(string, "I'm computing a piece of the answer")
+        6
+      }
+      val b = async {
+        log(string, "I'm computing another piece of the answer")
+        7
+      }
+      log(string, "The answer is ${a.await() * b.await()}")
+      showContent(string.toString())
+    }
+  }
+
+  private fun log(string: StringBuffer, msg: String) {
+    string.append("\n[${Thread.currentThread().name}] $msg")
+  }
+
+  private fun showContent(content: String) {
+    launch(UI) {
+      tv.append(content)
+    }
   }
 }
