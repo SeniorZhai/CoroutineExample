8d47bce28f9ff06b53ccf1928c72d31e57e56327
SeniorZhai <developer.zhaitao@gmail.com>
2018-09-28 20:42:22 +0800

    launch

--- app/src/test/java/com/coroutine/example/run/launch.kt
+++ app/src/test/java/com/coroutine/example/run/launch.kt
@@ -1,2 +1,32 @@
 package com.coroutine.example.run
 
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.delay
+import org.junit.Test
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.experimental.startCoroutine
+
+fun launch(context: CoroutineContext, block: suspend () -> Unit) = block.startCoroutine(
+    StandaloneCoroutine(context))
+
+private class StandaloneCoroutine(override val context: CoroutineContext) : Continuation<Unit> {
+  override fun resume(value: Unit) {}
+
+  override fun resumeWithException(exception: Throwable) {
+    val currentThread = Thread.currentThread()
+    currentThread.uncaughtExceptionHandler.uncaughtException(currentThread, exception)
+  }
+}
+
+class Launch {
+
+  @Test
+  fun launchTest() {
+    launch(GlobalScope.coroutineContext, {
+      delay(1000)
+      println("Done!!!")
+    })
+    Thread.sleep(2000)
+  }
+}
