ec6f75cec62b334dd3d2df99ec940d91bf58d13d
SeniorZhai <developer.zhaitao@gmail.com>
2018-09-28 20:42:35 +0800

    runBlocking

--- app/src/test/java/com/coroutine/example/lock/ReentrantLickTest.kt
+++ app/src/test/java/com/coroutine/example/lock/ReentrantLickTest.kt
@@ -1,2 +1,46 @@
 package com.coroutine.example.lock
 
+import org.junit.Test
+import java.util.concurrent.locks.ReentrantLock
+
+class ReentrantLickTest {
+
+  private val lock = ReentrantLock()
+
+  private val condition = lock.newCondition()
+
+  private fun await() {
+    try {
+      lock.lock()
+      println("await begin")
+      condition.await()
+      println("await end")
+    } finally {
+      lock.unlock()
+    }
+  }
+
+  private fun signal() {
+    try {
+      lock.lock()
+      println("signal begin")
+      condition.signal()
+      println("signal end")
+    } finally {
+      lock.unlock()
+    }
+  }
+
+  private val runnable = Runnable {
+    await()
+  }
+
+  @Test
+  fun testCondition() {
+    Thread(runnable).start()
+    Thread.sleep(3000)
+    signal()
+  }
+  // Condition.await()会释放锁 会等到signal都才会被唤醒
+
+}
--- app/src/test/java/com/coroutine/example/run/runBlocking.kt
+++ app/src/test/java/com/coroutine/example/run/runBlocking.kt
@@ -1,2 +1,59 @@
 package com.coroutine.example.run
 
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.delay
+import org.junit.Test
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.experimental.startCoroutine
+
+
+fun <T> runBlocking(context: CoroutineContext, block: suspend () -> T): T =
+    BlockingContinuation<T>(context).also { block.startCoroutine(it) }.getValue()
+
+private class BlockingContinuation<T>(override val context: CoroutineContext) : Continuation<T> {
+  private val lock = ReentrantLock()
+  private val done = lock.newCondition()
+  private var completed = false
+  private var value: T? = null
+  private var exception: Throwable? = null
+
+  private inline fun <T> locked(block: () -> T): T {
+    lock.lock()
+    return try {
+      block()
+    } finally {
+      lock.unlock()
+    }
+  }
+
+  override fun resume(value: T) = locked {
+    this.value = value
+    completed = true
+    done.signal()
+  }
+
+  override fun resumeWithException(exception: Throwable) {
+    this.exception = exception
+    completed = true
+    done.signal()
+  }
+
+  fun getValue(): T = locked {
+    while (!completed) done.awaitUninterruptibly() // await 等到Continuation执行完
+    exception?.let { throw it }
+    value as T
+  }
+}
+
+class RunBlocking {
+  @Test
+  fun blockingTest() {
+    val result = runBlocking(GlobalScope.coroutineContext) {
+      delay(1000)
+      "Hello world!"
+    }
+    println(result)
+  }
+}
